239e9623aced7e6a07383f9d9dfacb2e51134f54
huabingtao <1273621932@qq.com>
2020-02-26 21:50:44 +0800

    build

---
+++ build/precache-manifest.e682214ce854d88a2a29e764a8380568.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66df1dd1a9ca16833f62a7a5c4abfaeb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a02d987ef80df17871f",
+    "url": "./static/css/2.c35655eb.chunk.css"
+  },
+  {
+    "revision": "9df80bbba9fdd9e4bc5e",
+    "url": "./static/css/main.1639e668.chunk.css"
+  },
+  {
+    "revision": "8a02d987ef80df17871f",
+    "url": "./static/js/2.6dc2997f.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "./static/js/2.6dc2997f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df80bbba9fdd9e4bc5e",
+    "url": "./static/js/main.f9da7bb6.chunk.js"
+  },
+  {
+    "revision": "6698cc298a1e9e790618",
+    "url": "./static/js/runtime-main.a9534bd5.js"
+  }
+]);
