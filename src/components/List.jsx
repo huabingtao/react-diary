3ae82a5120955e5ca342f26bd4345a49327f811f
huabingtao <1273621932@qq.com>
2020-02-26 20:35:14 +0800

    modify 目录

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,89 +1,89 @@
-import React from 'react'
+import React from "react";
 
 class List extends React.Component {
   filterContent(content) {
     if (content.length < 50) {
-      return content
+      return content;
     }
-    return content.toString().substr(0, 65) + '...'
+    return content.toString().substr(0, 65) + "...";
   }
   render() {
     const itemStyle = {
       wrap: {
-        width: '100%',
-        padding: '0 0.8rem',
-        boxSizing: 'border-box'
+        width: "100%",
+        padding: "0 0.8rem",
+        boxSizing: "border-box"
       },
       item: {
-        borderRadius: '0.3rem',
-        boxShadow: 'rgba(0, 0, 0,0.2) 0 0.1rem 0.3rem 0.2rem',
-        padding: '0.8rem',
-        color: '#fff',
-        marginBottom: '0.8rem'
+        borderRadius: "0.3rem",
+        boxShadow: "rgba(0, 0, 0,0.2) 0 0.1rem 0.3rem 0.2rem",
+        padding: "0.8rem",
+        color: "#fff",
+        marginBottom: "0.8rem"
       },
       title: {
-        fontSize: '1.3rem',
-        padding: '0 0 0.8rem',
-        borderBottom: '0.05rem solid rgba(255,255,255,0.5)'
+        fontSize: "1.3rem",
+        padding: "0 0 0.8rem",
+        borderBottom: "0.05rem solid rgba(255,255,255,0.5)"
       },
       content: {
-        paddingTop: '1rem',
-        height: '4.2rem',
-        fontSize: '1rem',
-        lineHeight: '1.5rem',
-        marginBottom: '0.8rem',
-        display: 'flex',
-        alignItems: 'center'
+        paddingTop: "1rem",
+        // height: '4.2rem',
+        fontSize: "1rem",
+        lineHeight: "1.5rem",
+        marginBottom: "0.8rem",
+        display: "flex",
+        alignItems: "center"
       },
       feature: {
-        fontSize: '0.8rem',
-        display: 'flex',
-        justifyContent: 'space-between'
+        fontSize: "0.8rem",
+        display: "flex",
+        justifyContent: "space-between"
       },
       favor: {
-        display: 'flex',
-        alignItems: 'center'
+        display: "flex",
+        alignItems: "center"
       },
       abilityItemIconSvg: {
-        display: 'inline-block',
-        marginRight: '0.2rem',
-        width: '1.4rem',
-        height: '1.4rem',
-        marginTop: '0.1rem'
+        display: "inline-block",
+        marginRight: "0.2rem",
+        width: "1.4rem",
+        height: "1.4rem",
+        marginTop: "0.1rem"
       },
       time: {
-        display: 'flex',
-        alignItems: 'center'
+        display: "flex",
+        alignItems: "center"
       },
       bgColor1: {
-        background: '#b7c981'
+        background: "#b7c981"
       },
       bgColor2: {
-        background: '#b2a69c'
+        background: "#b2a69c"
       },
       bgColor3: {
-        background: '#ad8dc6'
+        background: "#ad8dc6"
       },
       bgColor4: {
-        background: '#bfb2a8'
+        background: "#bfb2a8"
       },
       bgColor5: {
-        background: '#8d847e'
+        background: "#8d847e"
       },
       bgColor6: {
-        background: '#a1807d'
+        background: "#a1807d"
       },
       bgColor7: {
-        background: '#88d0c0'
+        background: "#88d0c0"
       }
-    }
+    };
     return (
       <div style={itemStyle.wrap}>
         {this.props.list.map((item, index) => {
-          let copyStyle = JSON.parse(JSON.stringify(itemStyle.item))
-          let integer = (index % 7) + 1
-          integer = `bgColor${integer}`
-          Object.assign(copyStyle, itemStyle[integer])
+          let copyStyle = JSON.parse(JSON.stringify(itemStyle.item));
+          let integer = (index % 7) + 1;
+          integer = `bgColor${integer}`;
+          Object.assign(copyStyle, itemStyle[integer]);
           return (
             <div
               style={copyStyle}
@@ -114,11 +114,11 @@ class List extends React.Component {
                 <div style={itemStyle.time}>{item.create_time}</div>
               </div>
             </div>
-          )
+          );
         })}
       </div>
-    )
+    );
   }
 }
 
-export default List
+export default List;
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,43 +1,43 @@
-import axios from 'axios'
-import Qs from 'qs'
-import { Toast } from 'antd-mobile'
+import axios from "axios";
+import Qs from "qs";
+import { Toast } from "antd-mobile";
 
 const instance = axios.create({
-  baseURL: 'http://192.168.1.8:3000/',
+  baseURL: "http://139.196.94.6:3000/",
   timeout: 5000
-})
+});
 
 // request 拦截
 instance.interceptors.request.use(
   config => {
-    if (config.method === 'get') {
+    if (config.method === "get") {
       config.paramsSerializer = params => {
-        return Qs.stringify(params, { arrayFormat: 'brackets' })
-      }
+        return Qs.stringify(params, { arrayFormat: "brackets" });
+      };
     }
-    return config
+    return config;
   },
   error => {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
 // response 拦截
 instance.interceptors.response.use(
   response => {
     // 接口请求成功处理
-    return response
+    return response;
   },
   error => {
     // 提示给用户具体报错信息
-    let errObj = JSON.parse(JSON.stringify(error))
-    if (errObj.message === 'Network Error') {
-      Toast.fail('服务器异常请稍后再试', 2)
-      return Promise.reject()
+    let errObj = JSON.parse(JSON.stringify(error));
+    if (errObj.message === "Network Error") {
+      Toast.fail("服务器异常请稍后再试", 2);
+      return Promise.reject();
     }
-    Toast.fail(error.response.data.message, 2)
-    return Promise.reject(error)
+    Toast.fail(error.response.data.message, 2);
+    return Promise.reject(error);
   }
-)
+);
 
-export default instance
+export default instance;
